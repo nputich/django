183b1c88adaee272b5a7ea8d8b5286069c906073
Nathan Putich <nputich@gmail.com>
2026-06-07 04:19:42 -0400

    development UPDATE fix import

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,7 @@ from rest_framework import generics, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import AccessCode, Organization, Survey, SurveyAnswer, SurveyQuestion
+from .models import AccessCode, Note, Organization, Survey, SurveyAnswer, SurveyQuestion
 from .serializers import (
     NoteSerializer,
     OrganizationHubSerializer,
